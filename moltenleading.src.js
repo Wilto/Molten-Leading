bcb21f6f9e0ac18ada6b91512d05619dd1dc6c79
Mat Marquis <mat@matmarquis.com>
2012-02-06 11:56:08 -0500

    Added er’rything.

---
+++ moltenleading.src.js
@@ -0,0 +1,29 @@
+(function($) {
+	$.fn.moltenLeading = function( config ) {
+		var o = $.extend({
+				minline: 1.2,
+				maxline: 1.6,
+				minwidth: 320,
+				maxwidth: 1024
+			}, config ),
+			hotlead = function( el ) {
+				var $el = $( this !== window ? this : el ),
+					widthperc = parseInt( ( $el.width() - o.minwidth ) / ( o.maxwidth - o.minwidth ) * 100, 10 ),
+					linecalc = o.minline + ( o.maxline - o.minline ) * widthperc / 100;
+
+				if ( widthperc <= 0 || linecalc < o.minline ) {
+					linecalc = o.minline;
+				} else if ( widthperc >= 100 || linecalc > o.maxline ) {
+					linecalc = o.maxline;
+				}
+
+				$el.css( "lineHeight", linecalc );
+
+				$(window).one( "resize", function() {
+					hotlead( $el );
+				});
+			};
+
+		return this.each( hotlead );
+	};
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Slingin’ Hot Leading
+Manually adjusting ```line-height``` for optimum readability across a bunch of media queries is kind of a pain. So I made a thing.
+
+Binding to ```resize``` makes me nauseous to begin with, and in this first pass I’m going about it in a particularly gross way.
+
+I’m kinda bad at readmes.
